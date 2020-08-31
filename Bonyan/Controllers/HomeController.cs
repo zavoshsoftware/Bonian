5cd70cfb2523d9a959016fc3e86a545ead819956
zavoshsoftware <70507208+zavoshsoftware@users.noreply.github.com>
2020-08-31 15:29:19 +0430

    First push

---
+++ Bonyan/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bonyan.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [Route("")]
+        public ActionResult Index()
+        {
+            if (!Request.Browser.IsMobileDevice)
+            {
+                return RedirectToAction("IndexDesktop");
+            }
+            return View();
+        }
+        public ActionResult IndexDesktop()
+        {
+            return View();
+        }
+        [Route("details")]
+        public ActionResult Details()
+        {
+            if (!Request.Browser.IsMobileDevice)
+            {
+                return RedirectToAction("IndexDesktop");
+            }
+            return View();
+        }
+        [Route("author")]
+        public ActionResult Author()
+        {
+            if (!Request.Browser.IsMobileDevice)
+            {
+                return RedirectToAction("IndexDesktop");
+            }
+            return View();
+        }
+        [Route("profile")]
+        public ActionResult UserProfile()
+        {
+            if (!Request.Browser.IsMobileDevice)
+            {
+                return RedirectToAction("IndexDesktop");
+            }
+            return View();
+        }
+        [Route("search")]
+        public ActionResult Search()
+        {
+            if (!Request.Browser.IsMobileDevice)
+            {
+                return RedirectToAction("IndexDesktop");
+            }
+            return View();
+        }
+        [Route("like")]
+        public ActionResult Like()
+        {
+            if (!Request.Browser.IsMobileDevice)
+            {
+                return RedirectToAction("IndexDesktop");
+            }
+            return View();
+        }
+        [Route("about")]
+        public ActionResult About()
+        {
+            if (!Request.Browser.IsMobileDevice)
+            {
+                return RedirectToAction("IndexDesktop");
+            }
+            return View();
+        }
+    }
+}
